efb39b02bc2824f6f7414af3ea680c42593a68c9
Ricky Rivas <76985350+RickyRivas@users.noreply.github.com>
2022-02-01 23:52:32 -0600

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,48 @@
+<script>
+	import ContactCard from './ContactCard.svelte';
+
+  let name = "Ricky";
+  let description = 'Hardworking, intelligent, and persistent individual who is a servant of God!'
+  let jobTitle = 'Entrepreneaurer'
+  let age = 1;
+  let image = null
+  $: uppercaseName = name.toUpperCase()
+
+  $: console.log(name)
+  $: if(name === 'Ricky'){
+    age = 100
+  }
+
+  function addAge() {
+    return age++;
+  }
+  function changeName() {
+    name = 'Ricardo'
+  }
+  function nameInput(e){
+    const enteredValue = e.target.value;
+    name = enteredValue
+  }
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {uppercaseName}, I am age { age }!</h1>
+<button on:click="{addAge}">Change Age</button>
+<!-- <button on:click="{changeName}">Change Age</button> -->
+<!-- <input type="text" on:input='{nameInput}'> -->
+<input type="text" bind:value='{name}'>
+<input type="text" bind:value='{description}'>
+<input type="text" bind:value='{image}'>
+<input type="text" bind:value='{jobTitle}'>
+
+<ContactCard 
+userName='{name}'
+userDesc='{description}'
+userJobTitle='{jobTitle}'
+imgSrc='{image}'
+/>
